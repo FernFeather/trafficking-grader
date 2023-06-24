d46e5ce260947b64546e8a15cf550b56087be8fe
FernFeather <JulianaLucas5235@gmail.com>
2023-06-24 16:45:49 -0400

    add django santax

--- companies/urls.py
+++ companies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('companies', views.CompanyListView.as_view()),
+    path('companies', views.CompanyListView.as_view(), name="companies.list"),
     path('companies/<int:pk>', views.CompanyDetailView.as_view())
 ]
--- traffickingGrader/settings.py
+++ traffickingGrader/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'traffickingGrader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
