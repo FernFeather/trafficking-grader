c2b0c7d87b0c56fc6f17815bbe80c51abb9c758f
FernFeather <JulianaLucas5235@gmail.com>
2023-07-15 12:32:36 -0400

    add bbuttons to company individual

--- companies/urls.py
+++ companies/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('companies', views.CompanyListView.as_view(), name="companies.list"),
-    path('companies/<int:pk>', views.CompanyDetailView.as_view())
+    path('companies/<int:pk>', views.CompanyDetailView.as_view(), name="companies.detail")
 ]
