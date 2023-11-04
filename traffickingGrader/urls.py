7405be8bef97136f9bde3202c6eff734b1be6fa2
MUNING CHEN <muning.chen@ufl.edu>
2023-11-04 12:18:43 -0400

    changes in settings

--- traffickingGrader/urls.py
+++ traffickingGrader/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('comp/', include('companies.urls')),
-    path('about/', include('about.urls'))
+    path('about/', include('about.urls')),
+    path('apply/', include('apply.urls'))
 ]
