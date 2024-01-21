767e8e1eafdb2cc2ca812b1064713b0e33b9858c
FernFeather <JulianaLucas5235@gmail.com>
2024-01-20 21:43:08 -0500

    added company dropdown to nav bar. added companies to about.view for acess in the nav bar.

--- about/views.py
+++ about/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+# trafficking-grader/about/views.py
+from companies.models import Companies
+
 
 class AboutView(TemplateView):
     template_name = 'about/about.html'
-    extra_context = {'today': datetime.today()}
-
-# Create your views here.
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = datetime.today()
+        context['companies'] = Companies.objects.all()  # Add this line
+        return context
