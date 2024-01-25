93503e2def8e4370a75115a2926399cd59980ecf
ekuo3242@gmail.com <der-mingkuo@Der-Mings-MacBook-Air.local>
2024-01-25 11:51:18 -0500

    added header, banner, and image for home page

--- home/views.py
+++ home/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from companies.models import Companies
 class HomeView(TemplateView):
     template_name = 'home/welecome.html'
     extra_context = {'today': datetime.today()}
+    extra_context['companies'] = Companies.objects.all()
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
