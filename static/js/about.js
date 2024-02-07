8b4fc03c7a4a1e26ea355c16d3887646b3cd4aae
FernFeather <JulianaLucas5235@gmail.com>
2024-02-07 16:18:18 -0500

    carosel

--- static/js/about.js
+++ static/js/about.js
@@ -15,3 +15,38 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// carosel
+// JavaScript for Carousel
+var currentSlide = 0;
+var slides = document.getElementsByClassName("carousel-slide");
+
+// Function to show a slide
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
